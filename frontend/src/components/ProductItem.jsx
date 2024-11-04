8ab1da96d9bc2b1a6043c1dc29a525cc68903cb7
Dehelaan <pulkit22shiv@gmail.com>
2024-11-04 09:39:43 +0530

    fixed id syntax

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id,image,name,price}) => {
+  const ProductItem = ({id,image,name,price}) => {
 
-    const {currency}=useContext(ShopContext);
+  const {currency}=useContext(ShopContext);
 
   return (
-    <Link  className='text-gray-700 cursor pointer' to={`/product/ ${id}`}>
+    <Link  className='text-gray-700 cursor pointer' to={`/product/${id}`}>
         <div className='overflow-hidden'>
             <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
         </div>
